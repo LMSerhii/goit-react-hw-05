1febceac26c5c5baaa2a6c934b1dd428326d0674
LMSerhii <117682588+LMSerhii@users.noreply.github.com>
2024-02-10 11:39:37 +0200

    code separation

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,28 +1,44 @@
 import { Routes, Route } from 'react-router-dom';
+import { lazy, Suspense } from 'react';
 import style from './App.module.css';
-
-import HomePage from '../pages/HomePage';
-import MoviesPage from '../pages/MoviesPage';
-import MovieDetailsPage from '../pages/MovieDetailsPage';
-import NotFound from '../pages/NotFound';
+import { Bars } from 'react-loader-spinner';
 
 import { AppBar } from './AppBar';
 import { MovieCast } from './MovieCast';
 import { MovieReviews } from './MovieReviews';
 
+const HomePage = lazy(() => import('../pages/HomePage'));
+const MoviesPage = lazy(() => import('../pages/MoviesPage'));
+const MovieDetailsPage = lazy(() => import('../pages/MovieDetailsPage'));
+const NotFound = lazy(() => import('../pages/NotFound'));
+
 export const App = () => {
   return (
     <div className={style.container}>
       <AppBar />
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/movies" element={<MoviesPage />} />
-        <Route path="/movies/:movieId" element={<MovieDetailsPage />}>
-          <Route path="cast" element={<MovieCast />} />
-          <Route path="reviews" element={<MovieReviews />} />
-        </Route>
-        <Route path="*" element={<NotFound />} />
-      </Routes>
+
+      <Suspense
+        fallback={
+          <Bars
+            height="80"
+            width="80"
+            color="#747bff"
+            ariaLabel="bars-loading"
+            wrapperClass="loader"
+            visible={true}
+          />
+        }
+      >
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/movies" element={<MoviesPage />} />
+          <Route path="/movies/:movieId" element={<MovieDetailsPage />}>
+            <Route path="cast" element={<MovieCast />} />
+            <Route path="reviews" element={<MovieReviews />} />
+          </Route>
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Suspense>
     </div>
   );
 };
