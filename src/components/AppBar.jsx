dfa50169b49645539a77aab2dcdd1fda1c31196b
LMSerhii <117682588+LMSerhii@users.noreply.github.com>
2024-02-12 17:44:11 +0200

    fix route

--- src/components/AppBar.jsx
+++ src/components/AppBar.jsx
@@ -1,5 +1,7 @@
-import { NavLink, Link } from 'react-router-dom';
+import { Suspense } from 'react';
+import { NavLink, Link, Outlet } from 'react-router-dom';
 import { FaGun } from 'react-icons/fa6';
+import { Bars } from 'react-loader-spinner';
 
 import clsx from 'clsx';
 import style from './AppBar.module.css';
@@ -10,22 +12,38 @@ const buildLinkClass = ({ isActive }) => {
 
 export const AppBar = () => {
   return (
-    <header className={style.header}>
-      <Link to="/" className={style.logo}>
-        Evening
-        <span className={style.logoIcon}>
-          <FaGun />
-        </span>
-        movie
-      </Link>
-      <nav className={style.nav}>
-        <NavLink to="/" className={buildLinkClass}>
-          Home
-        </NavLink>
-        <NavLink to="/movies" className={buildLinkClass}>
-          Movies
-        </NavLink>
-      </nav>
-    </header>
+    <>
+      <header className={style.header}>
+        <Link to="/" className={style.logo}>
+          Evening
+          <span className={style.logoIcon}>
+            <FaGun />
+          </span>
+          movie
+        </Link>
+        <nav className={style.nav}>
+          <NavLink to="/" className={buildLinkClass}>
+            Home
+          </NavLink>
+          <NavLink to="/movies" className={buildLinkClass}>
+            Movies
+          </NavLink>
+        </nav>
+      </header>
+      <Suspense
+        fallback={
+          <Bars
+            height="80"
+            width="80"
+            color="#747bff"
+            ariaLabel="bars-loading"
+            wrapperClass="loader"
+            visible={true}
+          />
+        }
+      >
+        <Outlet />
+      </Suspense>
+    </>
   );
 };
